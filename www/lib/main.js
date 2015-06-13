88f39556a006c574995cea5dbd64c91645828c11
oscarlee <oscar3x39@gmail.com>
2015-06-13 17:49:08 +0800

    login > manager

--- www/lib/main.js
+++ www/lib/main.js
@@ -1,75 +1,85 @@
 jQuery(document).ready(function(event){
-  var isAnimating = false,
+
+    var url = 'http://10.10.0.159/';
+    var isAnimating = false,
     firstLoad = false;
 
-  //trigger smooth transition from the actual page to the new one
-  $('main').on('click', '[data-type="page-transition"]', function(event){
-    event.preventDefault();
-    //detect which page has been selected
-    var newPage = $(this).attr('href');
-    //if the page is not already being animated - trigger animation
-    if( !isAnimating ) changePage(newPage, true);
-    firstLoad = true;
-  });
+    $(document).keypress(function(e) {
+        $('.error').hide();
+        if(e.which == 13) {
+            $('.cd-btn').click();
+        }
+    });
 
-  //detect the 'popstate' event - e.g. user clicking the back button
-  $(window).on('popstate', function() {
-  	if( firstLoad ) {
-      /*
-      Safari emits a popstate event on page load - check if firstLoad is true before animating
-      if it's false - the page has just been loaded
-      */
-      var newPageArray = location.pathname.split('/'),
-        //this is the url of the page to be loaded
-        newPage = newPageArray[newPageArray.length - 1];
-      if( !isAnimating ) changePage(newPage, false);
+    $('main').on('click', '[data-type="page-transition"]', function(event){
+
+        event.preventDefault();
+        user = $('.cd-input[name="username"]').val();
+        pass = $('.cd-input[name="password"]').val();
+        if (user.length > 0 && pass.length > 0) {
+
+            var xhr = $.ajax({
+                crossDomain: true,
+                jsonpCallback: "ap",
+                dataType: "jsonp",
+                contentType: "application/json",
+                type: "POST",
+                url: url + '/admin/login.php',
+                data: "username="+user+"&password="+pass,
+                success: function(res){
+                    if (res.status == 'success') {
+                        loginSuccess(res.content.session);
+                    }
+
+                    if (res.status == 'failure') {
+                        $('.error').text(res.msg);
+                        $('.error').show();
+                    }
+                }
+            });
+            loginSuccess();
+
+        } else {
+            $('.error').show();
+        }
+    });
+
+    function loginSuccess(k)
+    {
+        document.cookie = 'k='+k;
+        var newPage = $('.cd-btn').attr('url');
+        if( !isAnimating ) changePage(newPage, true);
+        firstLoad = true;
     }
-    firstLoad = true;
+
+    $(window).on('popstate', function() {
+      	if( firstLoad ) {
+            var newPageArray = location.pathname.split('/'),
+                newPage = newPageArray[newPageArray.length - 1];
+            if( !isAnimating ) changePage(newPage, false);
+        }
+        firstLoad = true;
 	});
 
 	function changePage(url, bool) {
-    isAnimating = true;
-    // trigger page animation
-    $('body').addClass('page-is-changing');
-    $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-    	loadNewContent(url, bool);
-      $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-    });
-    //if browser doesn't support CSS transitions
-    if( !transitionsSupported() ) loadNewContent(url, bool);
+        isAnimating = true;
+        $('body').addClass('page-is-changing');
+        $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+
+            setTimeout(function(){
+                loadNewContent(url);
+            }, 1500);
+
+            $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+        });
+        if( !transitionsSupported() ) loadNewContent(url, bool);
 	}
 
-	function loadNewContent(url, bool) {
-		url = ('' == url) ? 'index.html' : url;
-  	var newSection = 'cd-'+url.replace('.html', '');
-  	var section = $('<div class="cd-main-content '+newSection+'"></div>');
+	function loadNewContent(url) {
+        location.replace(url);
+    }
 
-  	section.load(url+' .cd-main-content > *', function(event){
-      // load new content and replace <main> content with the new one
-      $('main').html(section);
-      //if browser doesn't support CSS transitions - dont wait for the end of transitions
-      var delay = ( transitionsSupported() ) ? 1200 : 0;
-      setTimeout(function(){
-        //wait for the end of the transition on the loading bar before revealing the new content
-        ( section.hasClass('cd-about') ) ? $('body').addClass('cd-about') : $('body').removeClass('cd-about');
-        $('body').removeClass('page-is-changing');
-        $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-          isAnimating = false;
-          $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-        });
-
-        if( !transitionsSupported() ) isAnimating = false;
-      }, delay);
-
-      if(url!=window.location && bool){
-        //add the new page to the window.history
-        //if the new page was triggered by a 'popstate' event, don't add it
-        window.history.pushState({path: url},'',url);
-      }
-		});
-  }
-
-  function transitionsSupported() {
-    return $('html').hasClass('csstransitions');
-  }
+    function transitionsSupported() {
+        return $('html').hasClass('csstransitions');
+    }
 });
