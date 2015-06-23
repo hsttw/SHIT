ae2df0b0bdb092343a5eacfdd783a501083678c4
VanceLin <mrjhack@hotmail.com>
2015-06-23 23:16:36 +0800

    add get content list feature

---
+++ www/admin/get_content_list.php
@@ -0,0 +1,80 @@
+<?php
+	require_once("connect.php");
+	require_once("lib.php");
+
+	$page_size = (isset($_REQUEST["page_size"]) && ($_REQUEST["page_size"] !== "")) ? (int) $_REQUEST["page_size"] : NULL;
+	$page_number = (isset($_REQUEST["page_number"]) && ($_REQUEST["page_number"] !== "")) ? (int) $_REQUEST["page_number"] : NULL;
+	$session = (isset($_REQUEST["s"]) && ($_REQUEST["s"] !== "")) ? (string) $_REQUEST["s"] : NULL;
+
+	$debug = false;
+	$respond = [];
+	$respond["status"] = "success";
+
+	if (is_null($session)) {
+		$respond["status"] = "failure";
+		$respond["msg"] = "error";
+	} else {
+		if (is_null($page_size)) {
+			$page_size = 10;
+		}
+
+		if (is_null($page_number)) {
+			$page_number = 1;
+		}
+
+		$db = db_connect();
+		if (!$db) {
+			$respond["status"] = "failure";
+			$respond["msg"] = "System down";
+		} else {
+			if (!verify_session($db, $session)) {
+				$respond["status"] = "failure";
+				$respond["msg"] = "error";
+			} else {
+				if (($query = $db->query("select count(*) as total from shit")) === false) {
+					$respond["status"] = "failure";
+					$respond["msg"] = ($debug ? "normal sql query failed: (" . $db->errorCode() . ") " . implode(",", $db->errorInfo()) : "error");
+				} else {
+					if (($result = $query->fetch(PDO::FETCH_ASSOC)) === false) {
+						$respond["status"] = "failure";
+						$respond["msg"] = ($debug ? "normal sql fetch failed: (" . $db->errorCode() . ") " . implode(",", $db->errorInfo()) : "error");
+					} else {
+						$total = (int) $result["total"];
+						$number_of_pages = ceil($total / $page_size);
+			    		$start = ($page_number - 1) * $page_size;
+			    	}
+				}
+
+				if ($respond["status"] == "success") {
+					$select_stmt =  $db->prepare("select username, email, password, browser_agent, device_id, create_date from shit limit :start, :page_size");
+					if (!$select_stmt) {
+						$respond["status"] = "failure";
+						$respond["msg"] = ($debug ? "select_stmt prepare failed: (" . $db->errorCode() . ") " . implode(",", $db->errorInfo()) : "error");
+					} else {
+						$select_1 = $select_stmt->bindParam(":start", $start, PDO::PARAM_INT);
+						$select_2 = $select_stmt->bindParam(":page_size", $page_size, PDO::PARAM_INT);
+						if (!$select_1 || !$select_2) {
+							$respond["status"] = "failure";
+							$respond["msg"] = ($debug ? "select_stmt bind failed: (" . $db->errorCode() . ") " . implode(",", $db->errorInfo()) : "error");
+						} else {
+							if (!$select_stmt->execute()) {
+								$respond["status"] = "failure";
+								$respond["msg"] = ($debug ? "select_stmt execute failed: (" . $db->errorCode() . ") " . implode(",", $db->errorInfo()) : "error");
+							} else {
+								if (($result = $select_stmt->fetchAll(PDO::FETCH_ASSOC)) === false) {
+									$respond["status"] = "failure";
+									$respond["msg"] = ($debug ? "select_stmt fetchAll failed: (" . $db->errorCode() . ") " . implode(",", $db->errorInfo()) : "error");
+								} else {
+									$respond["pager"]["total"] = $total;
+									$respond["pager"]["number_of_pages"] = $number_of_pages;
+									$respond["content"] = $result;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	echo "ap(" . json_encode($respond) . ")";
+?>
--- www/admin/lib.php
+++ www/admin/lib.php
@@ -1,8 +1,10 @@
 <?php
+	require_once("connect.php");
+
 	function setSession($username, $login_time) {
 		return sha1($username . $login_time);
 	}
-	/*I
+	/*
 	function password_verify($password, $crypt_password) {
 		if ($crypt_password === crypt($password, $crypt_password)) {
 			return true;
@@ -11,4 +13,51 @@
 		}
 	}
 	*/
+
+	function verify_session($db, $session) {
+
+		$valid_date = 1800; // 30分鐘
+		$result = true;
+		$now = date("Y-m-d H:i:s");
+		
+		$select_stmt = $db->prepare("select * from admin where session = :session");
+
+		if (!$select_stmt) {
+			$result = false;
+		} else {
+			if (!($select_stmt->bindParam(":session", $session, PDO::PARAM_STR))) {
+				$result = false;
+			} else {
+				if (!($select_stmt->execute())) {
+					$result = false;
+				} else {
+					if (($result = $select_stmt->fetch(PDO::FETCH_ASSOC)) === false) {
+						$result = false;
+					} else {
+						if ((strtotime($result["last_login_time"]) + $valid_date) < strtotime($now)) {
+							$result = false;
+						}
+					}
+				}
+			}
+		}
+
+		$update_stmt = $db->prepare("update admin set last_login_time = :last_login_time where session = :session");
+
+		if (!$update_stmt) {
+			$result = false;
+		} else {
+			$update_1 = $update_stmt->bindParam(":last_login_time", $now, PDO::PARAM_STR);
+			$update_2 = $update_stmt->bindParam(":session", $session, PDO::PARAM_STR);
+			if (!$update_1 || !$update_2) {
+				$result = false;
+			} else {
+				if (!$update_stmt->execute()) {
+					$result = false;
+				}
+			}
+		}
+
+		return $result;
+	}
 ?>
